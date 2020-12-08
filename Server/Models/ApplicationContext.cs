3438b86da56209e6cd94ed8c9aae978c185a8492
ferrari103 <erkin.t14@gmail.com>
2020-12-08 22:56:05 +0600

    Initial commit

---
+++ Server/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Server.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options)
+        { 
+            Database.EnsureCreated();
+        }
+ 
+        
+    }
+}
